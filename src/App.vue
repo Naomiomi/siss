9b1db9b215164279c93e08943b29d25ec2442cef
Naomi <nduran@certuit.com>
2019-11-19 23:58:27 -0800

    Inicio -toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,62 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <div>
+     <v-toolbar
+       :extended="extended"
+       :prominent="prominent"
+       :dense="dense"
+       :collapse="collapse"
+       :flat="flat"
+       :src="bg ? 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg' : undefined"
+       :extension-height="extensionHeight"
+     >
+       <template v-if="extendedSlot" #extension>
+         <v-toolbar-items>
+           <v-btn text>Link 1</v-btn>
+           <v-btn text>Link 2</v-btn>
+           <v-btn text>Link 3</v-btn>
+         </v-toolbar-items>
+       </template>
+
+       <v-toolbar-title
+       >Sistema Integral de Servicio Social</v-toolbar-title>
+
+       <v-spacer></v-spacer>
+
+       <v-toolbar-items>
+         <v-btn text>Taller de Inducción</v-btn>
+         <v-btn text>Programas de Servicio Social</v-btn>
+         <v-btn text>Configuración</v-btn>
+       </v-toolbar-items>
+
+       <template v-if="$vuetify.breakpoint.smAndUp">
+         <v-btn icon>
+           <v-icon>mdi-export-variant</v-icon>
+         </v-btn>
+       </template>
+     </v-toolbar>
+   </div>
+
+
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({
+      extended: true,
+      extendedSlot: false,
+      prominent: false,
+      dense: false,
+      collapse: false,
+      flat: true,
+      bg: true,
+      extensionHeight: 70,
+    }),
   }
-}
+</script>
+
+<style>
+  .change-font {
+    font-family: "Arial", Helvetica, sans-serif;
+  }
 </style>
